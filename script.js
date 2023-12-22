66a10da474d73e056feb24f28de33304f6aac315
Mormata <barbara.mor@outlook.com>
2023-12-22 17:06:33 -0300

    new

--- script.js
+++ script.js
@@ -3,13 +3,7 @@ function openModal(title, description, imagePath){
     var modalContent = document.getElementById('modal-content');
 
     modal.style.display = 'block';
-    modalContent.innerHTML = `
-    <h2> MEDIADORA CULTURAL NA 13ª BIENAL DE ARTES VISUAIS DO MERCOSUL (2022)
-    </h2>
-    <p> CURSO DE FORMAÇÃO EM MEDIAÇÃO "UM DIÁLOGO SINCERO"
-    <br> O Educativo da 13ª Bienal de Artes Visuais do Mercosul promoveu, entre julho e setembro de 2022, o curso de formação em mediação cultural "Um diálogo Sincero". A participação no curso era pré-requisito para a posterior seleção para o trabalho de mediação cultural nos espaços expositivos da 13ª Bienal de Artes Visuais do Mercosul. </p>
-    <img src="" alt="">
-    `;
+    modalContent.innerHTML = `${modal-content}`;
 }
 
 function closeModal(){
